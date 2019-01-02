bfba747349acaed06a4efeae23365c8d060e21bb
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-02 15:17:15 -0200

    Initializing the project

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Eduardo Fornari on 02/01/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
