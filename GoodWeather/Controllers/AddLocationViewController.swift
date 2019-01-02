f468c32b80c4d788053721cccb0056f5bd668091
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-02 15:47:50 -0200

    Setting Up View Controllers

---
+++ GoodWeather/Controllers/AddLocationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddLocationViewController.swift
+//  GoodWeather
+//
+//  Created by Eduardo Fornari on 02/01/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+
+}
---
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Eduardo Fornari on 02/01/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+
+}
