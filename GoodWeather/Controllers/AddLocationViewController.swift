eb41732a0b59e9a798bbe901253d0868685d1179
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-03 09:07:38 -0200

    Implementing WebService

--- GoodWeather/Controllers/AddLocationViewController.swift
+++ GoodWeather/Controllers/AddLocationViewController.swift
@@ -12,8 +12,17 @@ class AddLocationViewController: UIViewController {
 
     @IBOutlet private weak var locationTextField: UITextField!
 
-    @IBAction private func saveButtonDidTapped(){
-        
+    @IBAction private func saveButtonDidTapped() {
+        if let location = locationTextField.text {
+            if let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(location)&appid=f9e1e933016cf95809a531e8a4b032c8&units=imperial") {
+                let weatherResource = Resource<Any>(url: weatherURL) { data in
+                    return data
+                }
+                Webservice().load(resource: weatherResource) { result in
+                    
+                }
+            }
+        }
     }
 
     @IBAction private func cancelButtonDidTapped() {
