f3f1bab8ceafbfb2beaab1a4d32fc48d2f2cfd6b
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-04 12:41:42 -0200

    Implementing AddWeather Delegate

--- GoodWeather/Controllers/AddLocationViewController.swift
+++ GoodWeather/Controllers/AddLocationViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
+protocol AddWeatherDelegate: class {
+    func addWeatherDidSave(weatherViewModel: WeatherViewModel)
+}
+
 class AddLocationViewController: UIViewController {
 
     @IBOutlet private weak var locationTextField: UITextField!
+
+    public weak var delegate: AddWeatherDelegate?
 
     @IBAction private func saveButtonDidTapped() {
         if let location = locationTextField.text {
@@ -19,12 +25,17 @@ class AddLocationViewController: UIViewController {
                             "&appid=f9e1e933016cf95809a531e8a4b032c8" +
                             "&units=imperial"
             if let weatherURL = URL(string: urlString) {
-                let weatherResource = Resource<Any>(url: weatherURL) { data in
+                let weatherResource = Resource<WeatherViewModel>(url: weatherURL) { data in
                     let weatherViewModel = try? JSONDecoder().decode(WeatherViewModel.self, from: data)
                     return weatherViewModel
                 }
-                Webservice().load(resource: weatherResource) { result in
-                    
+                Webservice().load(resource: weatherResource) { [weak self] result in
+                    if let weatherViewModel = result {
+                        if let delegate = self?.delegate {
+                            delegate.addWeatherDidSave(weatherViewModel: weatherViewModel)
+                            self?.dismiss(animated: true)
+                        }
+                    }
                 }
             }
         }
