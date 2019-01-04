1592fccc78963aa6f95fcfeca4a9b7f9decb0a3c
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-04 19:03:42 -0200

    Displaying Settings Cells on the Settings Screen

--- GoodWeather/Controllers/SettingsTableViewController.swift
+++ GoodWeather/Controllers/SettingsTableViewController.swift
@@ -12,4 +12,28 @@ class SettingsTableViewController: UITableViewController {
 
     public static let segueIdentifier = String(describing: SettingsTableViewController.self)
 
+    private let settingsViewModel = SettingsViewModel()
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsViewModel.units.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let settingItem = settingsViewModel.units[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsTableViewCell", for: indexPath)
+        cell.textLabel?.text = settingItem.displayName
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
 }
