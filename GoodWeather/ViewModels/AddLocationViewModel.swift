26de00efefe778dbd1f58f8ffdf72472e365e0ca
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-04 23:45:49 -0200

    Creating AddLocationViewModel

---
+++ GoodWeather/ViewModels/AddLocationViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddLocationViewModel.swift
+//  GoodWeather
+//
+//  Created by Eduardo Fornari on 04/01/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import Foundation
+
+struct AddLocationViewModel {
+    var city: String = ""
+}
