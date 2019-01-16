844d0a2be976d665f27fcb5e2df76a58a9f0cf01
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2019-01-16 00:18:44 -0200

    Creating generic TableViewDataSource

---
+++ GoodWeather/Data Source/TableViewDataSource.swift
@@ -0,0 +1,38 @@
+//
+//  TableViewDataSource.swift
+//  GoodWeather
+//
+//  Created by Eduardo Fornari on 15/01/19.
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewDataSource<CellType, ViewModel>: NSObject, UITableViewDataSource where CellType: UITableViewCell {
+
+    private let cellIdentifier: String
+    private var items: [ViewModel]
+    private let configureCell: (CellType, ViewModel) -> Void
+
+    // MARK: - Init
+    init(cellIdentifier: String, items: [ViewModel], configureCell: @escaping (CellType, ViewModel) -> Void) {
+        self.cellIdentifier = cellIdentifier
+        self.items = items
+        self.configureCell = configureCell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            as? CellType else {
+            fatalError("Cell with identifier \(cellIdentifier) not found")
+        }
+        let viewModel = items[indexPath.row]
+        configureCell(cell, viewModel)
+        return cell
+    }
+}
